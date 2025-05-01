cb8d418d249afebf4d6025a7c6d65ccf02fe57bf
Shallange <jimmy.kroneld@hotmail.com>
2025-05-01 15:49:10 +0200

    Added features folder and environment file

---
+++ features/environment.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright
+
+# Called once before any tests run
+def before_all(context):
+    context.playwright = sync_playwright().start()
+    context.browser = context.playwright.chromium.launch(headless=False)
+
+# Called before every scenario (test case)
+def before_scenario(context, scenario):
+    context.page = context.browser.new_page()
+    context.base_url = "https://tap-ht24-testverktyg.github.io/exam-template/"
+
+# Called after every scenario
+def after_scenario(context, scenario):
+    context.page.close()
+
+# Called once after all tests are done
+def after_all(context):
+    context.browser.close()
+    context.playwright.stop()
